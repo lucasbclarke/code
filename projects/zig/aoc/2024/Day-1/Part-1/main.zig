a5d6b9b53509db30ca473e22fa9f2681f6d079aa
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-09-17 17:10:14 +1000

    started adding checks for different cases, starting to implament first and second lists along with a digit count to control the maths for index notation of the numbers

--- projects/zig/aoc/2024/Day-1/Part-1/main.zig
+++ projects/zig/aoc/2024/Day-1/Part-1/main.zig
@@ -2,6 +2,10 @@ const std = @import("std");
 const print = @import("std").debug.print;
 const embedfile = @embedFile("s");
 
+var first_list: bool = undefined;
+var second_list: bool = undefined;
+var digit_count: u32 = 0;
+
 pub fn main() !void {
     var buffer: [1000]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
@@ -18,14 +22,14 @@ pub fn main() !void {
         "4",
         "5",
         "6",
-        "7",
+        "7", 
         "8",
         "9",
         "",
         "\n",
     };
     const values = [_][]const u8 {
-        "zero",
+        "zero", 
         "one",
         "two",
         "three",
@@ -42,14 +46,66 @@ pub fn main() !void {
     for (keys, values) |key, value| {
         try map.put(key, value);
         print("key:value {s}:{s}\n", .{key, value});
-        //ask mr pike about why there newline is not working!
     }
 
-    var sequence = std.mem.splitAny(u8, embedfile, " \n");
-    while (sequence.next()) |seq1| {
-        if (map.get(seq1)) |word| {
-            print("{s}\n", .{word});
+    var lines = std.mem.splitAny(u8, embedfile, "\n");
+    while (lines.next()) |line| {
+        var sequence = std.mem.splitAny(u8, line, " ");
+        while (sequence.next()) |seq1| {
+            if (map.get(seq1)) |word| {
+                print("{s}\n", .{word});
+                if (std.mem.eql(u8, word, "space")) {
+                    if (first_list == true) {
+                        first_list == false;
+                    } else {
+                        print("this should not happen", .{});
+                    }
+                    digit_count = 0;
+                }
+                if (std.mem.eql(u8, word, "newline")) {
+                    if (second_list == true) {
+                        second_list == false;
+                    } else {
+                        print("this should not happen", .{});
+                    }
+                    digit_count = 0;
+                }
+                if (std.mem.eql(u8, word, "one")) {
+                    digit_count += 1;
+                    if (first_list == true) {
+                        //add the number to the index notation
+                        switch (digit_count) {
+                            '1' => { key * 1 };
+                            '2' => { key * 10 };
+                            '3' => { key * 100 };
+                            '4' => { key * 1000 };
+                            '5' => { key * 10,000 };
+                        }
+                    } else {
+                        second_list == true;
+                        //add the number to the index notation
+                    }
+                }
+                if (std.mem.eql(u8, word, "two")) {
+                    
+                }
+                if (std.mem.eql(u8, word, "three")) {
+                }
+                if (std.mem.eql(u8, word, "four")) {
+                }
+                if (std.mem.eql(u8, word, "five")) {
+                }
+                if (std.mem.eql(u8, word, "six")) {
+                }
+                if (std.mem.eql(u8, word, "seven")) {
+                }
+                if (std.mem.eql(u8, word, "eight")) {
+                }
+                if (std.mem.eql(u8, word, "nine")) {
+                }
+            }
         }
+        print("newline\n", .{});
     }
 
 }
