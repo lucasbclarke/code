cbc0ff775fc3022e3c670e803e6483669c0b6fee
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-11-26 12:08:14 +1100

    finished day-2/part 1, put comparison logic in a function and fixed issues with the previous number

--- projects/zig/aoc/2024/Day-2/Part-1/src/main.zig
+++ projects/zig/aoc/2024/Day-2/Part-1/src/main.zig
@@ -16,22 +16,45 @@ pub fn main() !void {
     var safe_reports: std.ArrayList(u32) = .empty;
     defer arr_list.deinit(allocator);
 
-    var file_buffer: [150]u8 = undefined; //change to 15,000 on final run
+    var file_buffer: [20000]u8 = undefined; //change to 15,000 on final run
     const file_reader = try std.fs.File.read(try std.fs.cwd().openFile("s", .{}), &file_buffer);
     _ = file_reader;
     for (file_buffer) |char| {
         print("char = {c}\n", .{char});
         if (char == ' ') {
+            
             if (running_total > 0) {
                 try arr_list.append(allocator, running_total);
                 //print("arry_list = {any}\n", .{arr_list.items});
             }
+            if (!new_line_started) { 
+                if (number_index > 0) {
+                    safe_report &= numberComparison(running_total, prev_number, &line_direction);
+                } else {
+                    print("previous number = running_total\n", .{});
+                    safe_report &= false;
+                }
+            }
+            print("report safe = {}\n", .{safe_report});
+            new_line_started = false;
+            prev_number = running_total;
             running_total = 0;
         } else if (char == '\n') {
+            if (!new_line_started) { 
+                if (number_index > 0) {
+                    safe_report &= numberComparison(running_total, prev_number, &line_direction);
+                } else {
+                    print("previous number = running_total\n", .{});
+                    safe_report &= false;
+                }
+            }
+            print("report safe = {}\n", .{safe_report});
+
             if (running_total > 0) {
                 try arr_list.append(allocator, running_total);
                 //print("arry_list = {any}\n", .{arr_list.items});
             }
+            prev_number = running_total;
             running_total = 0;
             report += 1;
             print("report = {}\n", .{report});
@@ -39,68 +62,66 @@ pub fn main() !void {
             if (safe_report) {
                 try safe_reports.append(allocator, report);
             }
+            prev_number = 0;
             safe_report = true;
             new_line_started = true;
             print("new_line_started\n", .{});
+            line_direction = 0;
         } else if (char >= '0' and char <= '9') {
             running_total *= 10;
             running_total += char - 48;
             
-            if (!new_line_started) { 
-                if (number_index > 0) {
-                    prev_number = arr_list.items[number_index - 1];
-
-                    if (prev_number != running_total) {
-                        print("prev number = {}\n", .{prev_number});
-                        print("running_total = {}\n", .{running_total});
-
-                        if (prev_number > running_total) {
-                            if (prev_number - running_total == 1 or
-                                prev_number - running_total == 2 or
-                                prev_number - running_total == 3) {
-                                print("subtraction between 1 and 3\n", .{});
-                                direction = -1;
-                                if (line_direction == 0 or line_direction == direction) {
-                                    safe_report &= true;
-                                    line_direction = direction;
-                                }
-                            } else {
-                                print("subtraction not between 1 and 3\n", .{});
-                                safe_report &= false;
-                            }
-                        } else if (prev_number < running_total) {
-                            if (running_total - prev_number == 1 or
-                                running_total - prev_number == 2 or
-                                running_total - prev_number == 3) {
-                                print("addition between 1 and 3 \n", .{});
-                                safe_report &= true;
-                                direction = 1;
-                                if (line_direction == 0 or line_direction == direction) {
-                                    safe_report &= true;
-                                    line_direction = direction;
-                                }
-                            } else {
-                                print("addition not between 1 and 3\n", .{});
-                                safe_report &= false;
-                            }
-                        } 
-                    } else {
-                        safe_report &= false;
-                    }
-                } else {
-                    print("previous number = running_total\n", .{});
-                    safe_report &= false;
-                }
-            } 
-
             number_index += 1;
-            print("report safe = {}\n", .{safe_report});
-            //print("number index = {}\n", .{number_index});
-            new_line_started = false;
         }
     }
 
     print("safe reports = {any}\n", .{safe_reports.items});
+    print("amount of safe reports = {d}\n", .{safe_reports.items.len});
     
     running_total = 0;
+}
+
+pub fn numberComparison(runningTotal: u32, prevNumber: u32, lineDirection: *i8) bool {
+    if (prevNumber != runningTotal) {
+        print("prev number = {}\n", .{prevNumber});
+        print("running_total = {}\n", .{runningTotal});
+
+        if (prevNumber > runningTotal) {
+            if (prevNumber - runningTotal == 1 or
+                prevNumber - runningTotal == 2 or
+                prevNumber - runningTotal == 3) {
+                print("subtraction between 1 and 3\n", .{});
+                direction = -1;
+                if (lineDirection.* == 0 or lineDirection.* == direction) {
+                    safe_report &= true;
+                    lineDirection.* = direction;
+                } else {
+                    safe_report &= false;
+                }
+            } else {
+                print("subtraction not between 1 and 3\n", .{});
+                safe_report &= false;
+            }
+        } else if (prevNumber < runningTotal) {
+            if (runningTotal - prevNumber == 1 or
+                runningTotal - prevNumber == 2 or
+                runningTotal - prevNumber == 3) {
+                print("addition between 1 and 3 \n", .{});
+                safe_report &= true;
+                direction = 1;
+                if (lineDirection.* == 0 or lineDirection.* == direction) {
+                    safe_report &= true;
+                    lineDirection.* = direction;
+                } else {
+                    safe_report &= false;
+                }
+            } else {
+                print("addition not between 1 and 3\n", .{});
+                safe_report &= false;
+            }
+        } 
+    } else {
+        safe_report &= false;
+    }
+    return safe_report;
 }
